8f67d985463a9433246f2deef643aee1d1ccb469
Bruno Russi Lautenschlager <bruno.lautenschlager@ciss.com.br>
2019-05-24 12:06:54 -0300

    feat: Ajusted to use components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import PokerForm from "./components/PokerForm";
+import * as Api from "./service/api/PokerApi";
+
+const MainContainer = styled.div`
+  padding: 2em calc(50% - 460px);
+`;
+
+const App = () => {
+  const handleSubmit = values => {
+    Api.checkHand(...values).then(result => {
+      console.log(result);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContainer>
+      <PokerForm handleSubmit={handleSubmit} />
+    </MainContainer>
   );
-}
+};
 
 export default App;
