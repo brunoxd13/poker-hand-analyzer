79cd4ae72d1d3a1f8d27d60a4429c1f977f1bea3
Bruno Russi Lautenschlager <brunoxd13@gmail.com>
2019-05-24 19:58:06 -0300

    feat: compute tie on frontend

--- frontend/src/components/PokerResult/index.js
+++ frontend/src/components/PokerResult/index.js
@@ -3,7 +3,12 @@ import Label from "../Label";
 
 const PokerResult = ({ handWinner, handOneDescr, handTwoDescr }) => (
   <>
-    <Label>{handWinner > 0 && `The hand winner is: ${handWinner}`}</Label>
+    <Label>
+      {handWinner > 0 &&
+        (handWinner > 2
+          ? `The no winner is a tie`
+          : `The hand winner is: ${handWinner}`)}
+    </Label>
     <Label>{handOneDescr && `The hand one is: ${handOneDescr}`}</Label>
     <Label>{handTwoDescr && `The hand two is: ${handTwoDescr}`}</Label>
   </>
